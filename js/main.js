97966ba51c2fef9fef265fbe8e1690ea4aaf25c8
viktor-gif <klim.victor.ua@gmail.com>
2021-10-03 12:19:55 +0300

    fixing some bug

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 (function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
+        }
+    };
+}());
+
+(function () {
     const burger = document.querySelector('.burger');
     const menu = document.querySelector('.header_nav');
     const smallMenuClose = document.querySelector('.header_nav_close')
@@ -7,5 +18,5 @@
     })
     smallMenuClose.addEventListener('click', () => {
         menu.classList.remove('header_nav_active');
-    })
+    });
 }());
